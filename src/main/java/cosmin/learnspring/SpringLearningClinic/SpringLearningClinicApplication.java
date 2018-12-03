142ea910cff4873da36d75a272c0f6e467342ddb
Cosmin Tudora <cosmintudora1@gmail.com>
2018-12-03 15:45:35 +0200

    first commit

---
+++ src/main/java/cosmin/learnspring/SpringLearningClinic/SpringLearningClinicApplication.java
@@ -0,0 +1,12 @@
+package cosmin.learnspring.SpringLearningClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearningClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearningClinicApplication.class, args);
+	}
+}
---
+++ src/test/java/cosmin/learnspring/SpringLearningClinic/SpringLearningClinicApplicationTests.java
@@ -0,0 +1,16 @@
+package cosmin.learnspring.SpringLearningClinic;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringLearningClinicApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
